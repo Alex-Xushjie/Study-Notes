ce6c20275cd1f86c577e60d07df34807d43c71da
Alex Xu <alexshengchieh@gmail.com>
2025-08-31 16:22:27 +0800

    update

--- NetDevOps/Python_Basic/test.py
+++ NetDevOps/Python_Basic/test.py
@@ -5,10 +5,10 @@ with open(template_file) as template:
     fsm = textfsm.TextFSM(template)
 
     raw_data = """
-    Interface   Status  Description
-    Gi1/0/1    up      Server1
-    Gi1/0/2    down    Printer
-    Gi1/0/3    up      Switch
+Interface   Status  Description
+Gi1/0/1    up      Server1
+Gi1/0/2    down    Printer
+Gi1/0/3    up      Switch
     """
 parsed_data = [dict(zip(fsm.header.row)) for row in results]
 print(parsed_data)
