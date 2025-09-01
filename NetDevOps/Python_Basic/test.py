efc4cb08cccbff7e0dbd4141c9b0420cdf5f3c07
Alex Xu <alexshengchieh@gmail.com>
2025-09-01 23:08:55 +0800

    update

--- NetDevOps/Python_Basic/test.py
+++ NetDevOps/Python_Basic/test.py
@@ -4,13 +4,12 @@ template_file = "/Users/alex_xu/Study-Notes/NetDevOps/Python_Basic/interface.tem
 with open(template_file) as template:
     fsm = textfsm.TextFSM(template)
     raw_data = """
-    Interface   Status  Description
-    Gi1/0/1    up      Server1
-    Gi1/0/2    down    Printer
-    Gi1/0/3    up      Switch
-    """
+Interface   Status  Description
+Gi1/0/1    up      Server1
+Gi1/0/2    down    Printer
+Gi1/0/3    up      Switch
+"""
     results = fsm.ParseText(raw_data)
-
-parsed_data = [dict(zip(fsm.header.row)) for row in results]
+parsed_data = [dict(zip(fsm.header,row)) for row in results]
 print(parsed_data)
 
