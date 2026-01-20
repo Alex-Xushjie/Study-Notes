9eb22b9cd7d453bc23b3296652d0e3dc244f3010
Alex Xu <alexshengchieh@gmail.com>
2026-01-20 18:03:40 +0800

    update

--- NetDevOps/test.py
+++ NetDevOps/test.py
@@ -1,13 +1,17 @@
-from netmiko import ConnectHandler
+# from netmiko import ConnectHandler
 
-device = {
-    'device_type': 'cisco_ios',
-    'host': '10.160.14.9',
-    'username': 'cisco',
-    'password': 'Cisco123'}
-net_connect = ConnectHandler(**device)
-output = net_connect.send_command('show ip int brief')
-print(output)
-net_connect.disconnect()
+# device = {
+#     'device_type': 'cisco_ios',
+#     'host': '10.160.14.9',
+#     'username': 'cisco',
+#     'password': 'Cisco123'}
+# net_connect = ConnectHandler(**device)
+# output = net_connect.send_command('show ip int brief')
+# print(output)
+# net_connect.disconnect()
 
+import time
 
+cmd = input('C:\Administrator> ')
+ip_str = cmd.split('ping')[1].strip()
+print(f'来自 {ip_str} 的回复: 请求超时')
